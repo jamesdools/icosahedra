406d0e28ff0291ac290d11385c1a1371839ceb41
James Dooley <james.dooley@bbc.co.uk>
2017-07-31 23:02:32 +0100

    Added vertices morphing

--- shapes.js
+++ shapes.js
@@ -3,6 +3,7 @@ var raycaster, mouse, vector;
 var controls, zooming;
 var meshes;
 var webGL = false;
+var animate = false;
 
 function setRenderer() {
   document.querySelector('.three-canvas').innerHTML = '';
@@ -14,21 +15,39 @@ function setRenderer() {
     });
     webGL = true;
 
-    document.querySelector('.control').style.backgroundColor = 'black';
-    document.querySelector('.credit').style.backgroundColor = 'black';
+    document.querySelector('.bottom-panel').style.backgroundColor = 'black';
   }
   else {
     renderer = new THREE.CanvasRenderer();
     webGL = false;
 
-    document.querySelector('.control').style.background = 'none';
-    document.querySelector('.credit').style.background = 'none';
+    document.querySelector('.bottom-panel').style.background = 'none';
   }
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.querySelector('.three-canvas').appendChild(renderer.domElement);
 }
 
+function toggleAnimate() {
+  if (animate) {
+    animate = false;
+    return;
+  }
+
+  animate = true;
+}
+
+function onDocumentMouseMove(event) {
+  event.preventDefault();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
 function setup() {
   if (!Detector.webgl) Detector.addGetWebGLMessage();
 
@@ -52,8 +71,10 @@ function setup() {
   raycaster.params.Points.threshold = 0.1;
 
   meshes = [];
-
   for (var i = 0; i < 10; i++) addNewShape();
+
+  document.addEventListener('mousemove', onDocumentMouseMove, false );
+  window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function updateZoom() {
@@ -70,12 +91,10 @@ function updateZoom() {
 
 function addNewShape() {
   var geometry = new THREE.IcosahedronGeometry((Math.random() * 150), Math.floor(Math.random() * 3));
-  var material = new THREE.MeshBasicMaterial({wireframe: true, wireframeLinejoin: 'miter', skinning: true});
-  material.color.setRGB(Math.random(), Math.random(), Math.random());
-
+  var material = new THREE.MeshBasicMaterial({wireframe: true, wireframeLinejoin: 'miter', skinning: true, morphTargets: true});
   var mesh = new THREE.Mesh(geometry, material);
-  mesh.morphTargets = true;
-  generateMorphTargets(mesh.geometry);
+
+  material.color.setRGB(Math.random(), Math.random(), Math.random());
 
   scene.add(mesh);
   meshes.push(mesh);
@@ -94,12 +113,14 @@ function draw() {
     mesh.rotation.x += 0.01;
   });
 
-  raycaster.set(camera.position, vector.sub(camera.position).normalize());
-  var intersections = raycaster.intersectObjects(meshes);
+  // raycaster.set(camera.position, vector.sub(camera.position).normalize());
+  raycaster.setFromCamera(mouse, camera);
 
+  var intersections = raycaster.intersectObjects(scene.children);
   if (intersections.length > 0 && intersections[0].object.type === 'Mesh') {
     var mesh = intersections[0].object;
-    updateMorphs(mesh);
+
+    if (animate) animateMesh(mesh);
   }
 
   updateZoom();
@@ -112,28 +133,16 @@ window.onload = () => {
   draw();
 };
 
-function updateMorphs (mesh) {
-	return function () {
-    mesh.updateMorphTargets();
-    mesh.geometry.verticesNeedUpdate = true;
-    mesh.geometry.morphTargetsNeedUpdate = true;
-    mesh.material.needsUpdate = true;
-	};
-}
+function animateMesh(mesh) {
+  mesh.geometry.vertices.forEach((point) => {
+    scale = 1.01 - Math.random() * 0.02;
 
-function generateMorphTargets (geometry) {
-	var vertices = [], scale;
-	for (var i = 0; i < geometry.vertices.length; i++) {
-		vertices.push(geometry.vertices[ i ].clone());
-
-		scale = 1 + Math.random() * 0.3;
-		vertices[ vertices.length - 1 ].x *= scale;
-		vertices[ vertices.length - 1 ].y *= scale;
-		vertices[ vertices.length - 1 ].z *= scale;
-	}
-
-	geometry.morphTargets.push({
-    name: "target1",
-    vertices: vertices
+    point.x *= scale;
+    point.y *= scale;
+    point.z *= scale;
   });
+
+  mesh.geometry.verticesNeedUpdate = true;
+  mesh.geometry.elementsNeedUpdate = true;
+  mesh.geometry.morphTargetsNeedUpdate = true;
 }
